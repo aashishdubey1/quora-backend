df5e14c23f2d29445d626eebfa0f4892a6f400f8
Aashish <aashishdubey.dev@gmail.com>
2025-06-13 13:48:37 +0530

    Feat: added comment route

--- src/routes/v1/comment-routes.js
+++ src/routes/v1/comment-routes.js
@@ -1,1 +1,17 @@
 const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/:id',()=>{/* get a specific comment */})
+
+router.put('/:id',()=>{/* update a specific comment */})
+
+router.delete('/:id',()=>{/*delete a specific comment */})
+
+
+router.post('/:id/comments',()=>{/* add comment to specific comment */})
+
+router.get('/:id/comments',()=>{/* get comments to specific comment */})
+
+module.exports = router;
