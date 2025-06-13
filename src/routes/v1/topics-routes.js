3c2adaa9583ea3ce1e03f895b506b324d5b75e14
Aashish <aashishdubey.dev@gmail.com>
2025-06-13 13:52:09 +0530

    Feat: added topic route

---
+++ src/routes/v1/topics-routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/',()=>{/* create a new topic */})
+
+router.get('/',()=>{/*get all topics */})
+
+router.delete('/:id',()=>{/* delete a topic */})
+
+module.exports = router
+
